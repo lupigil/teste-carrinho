de2d49847e1bbf0a5af3b1aa00fce31375ff295c
Gilson <gilsontorresr@hotmail.com>
2021-10-05 21:32:54 -0300

    listar feito

--- site/src/routes.js
+++ site/src/routes.js
@@ -1,12 +1,14 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Teste from './infoX/home/index'
+
 
 export default function Routes() {
   return (
       <BrowserRouter>
           <Switch>
-              <Route path="/" exact={true} component={} />
+              <Route path="/" exact={true} component={Teste} />
           </Switch>
       </BrowserRouter>
   )
