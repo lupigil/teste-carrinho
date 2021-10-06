70617feed5a5ba9741e295be00da4c3a6294486d
Gilson <gilsontorresr@hotmail.com>
2021-10-06 15:25:26 -0300

    faltando função qtd

--- site/src/routes.js
+++ site/src/routes.js
@@ -1,14 +1,18 @@
 
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Teste from './infoX/home/index'
+import Home from './infoX/home/index'
+import Detalhes from './infoX/detalheProduto/index'
+import Carrinho from './infoX/carrinho/index'
 
 
 export default function Routes() {
   return (
       <BrowserRouter>
           <Switch>
-              <Route path="/" exact={true} component={Teste} />
+              <Route path="/" exact={true} component={Home} />
+              <Route path="/detalhe"  component={Detalhes} />
+              <Route path="/carrinho"  component={Carrinho} />
           </Switch>
       </BrowserRouter>
   )
